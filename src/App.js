be714848f12850f7fe0e1e33a691269c876bcf00
kauandemello97@gmail.com <kauandemello97@gmail.com>
2023-06-29 19:12:38 -0300

    style: container and center container box added, responsive style too

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <Container>
+      <CenterContainer>
+        Olá, tudo bem?
+      </CenterContainer>
+    </Container>
+  )
 }
 
-export default App;
+
+const Container = styled.div`
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: black;
+    position: fixed;
+`
+
+const CenterContainer = styled.div`
+    height: 800px;
+    width: 85%;
+    border-radius: 10px;
+    display: flex;
+    margin: 10px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    
+`
